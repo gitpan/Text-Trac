15c67de27a3c14ce595130b0d3350898e4854187
Gosuke Miyashita <gosukenator@gmail.com>
2006-07-19 12:32:11 +0000

    Import of MIZZY/Text-Trac-0.04 from CPAN.
    gitpan-cpan-distribution: Text-Trac
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         MIZZY/Text-Trac-0.04.tar.gz
    gitpan-cpan-author:       MIZZY
    gitpan-cpan-maturity:     released

--- lib/Text/Trac/AutoLinkHttpNode.pm
+++ lib/Text/Trac/AutoLinkHttpNode.pm
@@ -4,36 +4,24 @@ use strict;
 
 my $url_regex = 'https?:\/\/[A-Za-z0-9~\/._\?\&=\-%#\+:\;,\@\']+[A-Za-z0-9~\/_\?\&=\-%#\+:\;,\@\']';
 
-my $pattern1 = "($url_regex)";
-my $pattern2 = "\\[ ($url_regex) \\s+ ([^\\]]+) \\]";
-
 sub init {
     my $self = shift;
-    $self->pattern(qr/ $pattern1 | $pattern2 /xms);
+    $self->pattern(qr/ $url_regex /xms);
     return $self;
 }
 
 sub parse {
     my ( $self, $l ) = @_;
-    my $c = $self->{context};
+
     my $pattern = $self->pattern;
+    $l =~ $pattern or return $l;
 
-    my $copy_of_l = $l;
+    $l =~ s{(^|\s+)($url_regex)}{$1\[$2 $2\]}xmsg;
 
-    while ( $l =~ /$pattern/g ){
-        my $match = $2 || $1;
-        if ( $match =~ /\.(png|gif|jpg)$/ ){
-            $copy_of_l =~ s{ $pattern2 }{<img src="$1" alt="$2" />}xmsg;
-            $copy_of_l =~ s{ $pattern1 }{<img src="$1" alt="$1" />}xmsg unless $& =~ /^\[/;
-        }
-        else {
-            $copy_of_l =~ s{ $pattern2 }{<a class="ext-link" href="$1">$2</a>}xmsg;
-            $copy_of_l =~ s{ $pattern1 }{<a class="ext-link" href="$1">$1</a>}xmsg unless $& =~ /^\[/;
-        }
+    $l =~ s{\[([^\]]+ \. (?: png | gif | jpg ) ?)\ ([^\]]+?)\] }{<img src="$1" alt="$2" />}xmsg;
+    $l =~ s{\[([^\]]+?)\ ([^\]]+?)\]}{<a class="ext-link" href="$1">$2</a>}xmsg;
 
-    }
-
-    return $copy_of_l;
+    return $l;
 }
 
 1;
